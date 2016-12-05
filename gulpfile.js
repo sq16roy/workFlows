1fd2e4ae4b21162bd356182c0631e89f113a2a7c
¨roy <sq16roy@gmail.com>
2016-12-05 15:51:41 -0600

    add concat plug

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,24 @@
 var gulp = require('gulp'),
-    gutil = required('gulp-util'),
-    coffee = required('gulp-coffee');
+    gutil = require('gulp-util'),
+    coffee = require('gulp-coffee'),
+    concat = require('gulp-concat');
+
 var coffeeSources = ['components/coffee/*.coffee'];
+var jsSources = [
+    'components/scripts/rclick.js',
+    'components/scripts/pixgrid.js',
+    'components/scripts/tagline.js',
+    'components/scripts/template.js'
+];
 gulp.task('coffee', function(){
     gutil.src(coffeeSources)
         .pipe(coffee({ bare: true })
             .on('error', gutil.log))
         .pipe(gulp.dest('components/scripts'))
 });
+
+gulp.task('js', function(){
+    gulp.src(jsSources)
+    .pipe(concat('scripts.js'))
+    .pipe(gulp.dest('builds/development/js'))
+})
