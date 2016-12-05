ca0037f967f8576669f5d7327b2d3539d4760a87
¨roy <sq16roy@gmail.com>
2016-12-05 15:39:15 -0600

    added coffee

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,10 @@
 var gulp = require('gulp'),
-    gutil = required('gulp-util');
-gulp.task('log', function(){
-    gutil.log('Workflows are awesome');
+    gutil = required('gulp-util'),
+    coffee = required('gulp-coffee');
+var coffeeSources = ['components/coffee/*.coffee'];
+gulp.task('coffee', function(){
+    gutil.src(coffeeSources)
+        .pipe(coffee({ bare: true })
+            .on('error', gutil.log))
+        .pipe(gulp.dest('components/scripts'))
 });
