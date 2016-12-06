93d876abc5547a62ff655935134e0d7c582fefb4
¨roy <sq16roy@gmail.com>
2016-12-06 08:34:45 -0600

    added browserify

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,10 @@
 var gulp = require('gulp'),
     gutil = require('gulp-util'),
     coffee = require('gulp-coffee'),
+    browserify = require('gulp-browserify'),
     concat = require('gulp-concat');
 
-var coffeeSources = ['components/coffee/*.coffee'];
+var coffeeSources = ['components/coffee/tagline.coffee'];
 var jsSources = [
     'components/scripts/rclick.js',
     'components/scripts/pixgrid.js',
@@ -11,7 +12,7 @@ var jsSources = [
     'components/scripts/template.js'
 ];
 gulp.task('coffee', function(){
-    gutil.src(coffeeSources)
+    gulp.src(coffeeSources)
         .pipe(coffee({ bare: true })
             .on('error', gutil.log))
         .pipe(gulp.dest('components/scripts'))
@@ -20,5 +21,6 @@ gulp.task('coffee', function(){
 gulp.task('js', function(){
     gulp.src(jsSources)
     .pipe(concat('scripts.js'))
+    .pipe(browserify())
     .pipe(gulp.dest('builds/development/js'))
-})
+});
